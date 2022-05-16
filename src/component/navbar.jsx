b79ab1315aa966bb9f775b689d76f294ce4c5622
Vivek Goswami <vivekgoswamidbng@gmail.com>
2022-05-16 22:13:33 +0530

    update

---
+++ src/component/navbar.jsx
@@ -0,0 +1,23 @@
+// import React from "react";
+//import Navbar.css
+
+
+  function Navbar(){
+    return (
+      <div className="main">
+        <div className="first"> <h3>LOGOBAKERY</h3>
+        </div>
+        <div className="second">
+           <h5>Service  </h5>
+           <h5>Projects  </h5>
+           <h5>About</h5>
+           </div>
+        <div className="third">
+          <button>Contact</button>
+        </div>
+      </div>
+    )
+  }
+ 
+
+ export default Navbar;
---
+++ src/component/static.jsx
@@ -0,0 +1,28 @@
+
+
+let Statics = () => {
+    return (
+      <div className="data">
+          <div>
+              <h2>Mobile Operating System</h2>
+              <ul>
+                  <li>Android</li>
+                  <li>BlackBerry</li>
+                  <li>Iphone</li>
+                  <li>Windows Phone</li>
+              </ul>
+          </div>
+          <div>
+              <h2>Mobile Manufactures</h2>
+              <ul>
+                  <li>Samsung</li>
+                  <li>HTC</li>
+                  <li>Micromax</li>
+                  <li>Apple </li>
+              </ul>
+          </div>
+      </div>
+    )
+}
+
+export default Statics;
